e715ed1685083fbd6eda5694c0b1ed4c3b9331a3
ManpreetKaur2102 <manpreetkahlon2102@gmail.com>
2024-07-29 22:09:36 -0400

    Add files via upload

---
+++ script10.js
@@ -0,0 +1,7 @@
+var itemCount = 0;
+function addToList(){
+    itemCount++;
+    var listItem = document.createElement('li');
+    listItem.innerText = 'Item ' + itemCount;
+    document.getElementById('itemList').appendChild(listItem);
+}
